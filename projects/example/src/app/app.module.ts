abd75b790ba0216bbe910b3fc4c8941e0dfbaf63
Matias Camiletti <matias.camiletti@gmail.com>
2023-02-08 12:13:15 -0300

    Import library in example.

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TotsFilterBoxModule } from 'projects/tots/filter-box/src/public-api';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    TotsFilterBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
